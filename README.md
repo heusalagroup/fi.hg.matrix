0b915bc9b30a6fa08aade5a944f4152d37a30ba5
Jaakko Heusala <jheusala@iki.fi>
2021-08-19 01:29:17 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# matrix
-Lightweight Matrix Typescript Library
+# @sendanor/matrix
+
+Our lightweight Matrix.org library written in TypeScript.
+
+### Why?
+
+The official SDK was too complex and bloat for OpenWRT devices.
+
+### It's MIT licenced
+
+### It doesn't have many runtime dependencies
+
+This library expects [@sendanor/typescript](https://github.com/sendanor/typescript) to be located 
+in the relative path `../ts` and only required dependency it has is for [Lodash 
+library](https://lodash.com/).
+
+### It's well tested
+
+Our unit tests exists beside the code. To run tests, check out our test repository 
+[@sendanor/test](https://github.com/sendanor/test).
+
+### We don't have traditional releases
+
+This project evolves directly to our git repository in an agile manner.
+
+This git repository contains only the source code for compile time use case. It is meant to be used 
+as a git submodule in a NodeJS or webpack project.
+
+Recommended way to initialize your project is like this:
+
+```
+mkdir -p src/nor
+
+git submodule add git@github.com:sendanor/typescript.git src/nor/ts
+git config -f .gitmodules submodule.src/nor/ts.branch main
+
+git submodule add git@github.com:sendanor/matrix.git src/nor/pipeline
+git config -f .gitmodules submodule.src/nor/pipeline.branch main
+```
+
+Only required dependency is to [the Lodash library](https://lodash.com/):
+
+```
+npm install --save-dev lodash @types/lodash
+```
+
+Some of our code may use reflect metadata. It's optional otherwise.
+
+```
+npm install --save-dev reflect-metadata
+```
+
+### Stable releases available for a commercial customer
+
+For *tailored commercial release*, you may contact [our sales](mailto:info@sendanor.fi).
+
+General rule for pricing is 500 € (or $600) / [feature](https://github.com/sendanor/matrix/issues). 
+
+One full stable release containing multiple components is 8000 €. The full payment includes a month 
+of agile development with the customer, and a year of support for that release branch.
---
+++ SimpleMatrixClient.ts
@@ -0,0 +1,255 @@
+import Observer, { ObserverCallback, ObserverDestructor } from "./nor/ts/Observer";
+import RequestClient from "./nor/ts/RequestClient";
+import LogService from "./nor/ts/LogService";
+import { forEach, keys } from "./nor/ts/modules/lodash";
+import JsonAny from "./nor/ts/Json";
+
+const LOG = LogService.createLogger('SimpleMatrixClient');
+
+export enum SimpleMatrixClientEvent {
+
+    EVENT = "SimpleMatrixClient:event"
+
+}
+
+export type SimpleMatrixClientDestructor = ObserverDestructor;
+
+export interface MatrixEventContentDTO {
+    readonly body    : string;
+    readonly msgtype : string;
+}
+
+export interface MatrixEventDTO {
+
+    readonly content          : MatrixEventContentDTO;
+    readonly room_id          : string;
+    readonly event_id         : string;
+    readonly origin_server_ts : number;
+    readonly sender           : string;
+    readonly type             : string;
+
+}
+
+export interface MatrixTextMessageDTO {
+
+    readonly msgtype          : string;
+    readonly body             : string;
+
+}
+
+/**
+ * Super simple matrix event listener.
+ *
+ * Far from perfect, but works on OpenWRT and NodeJS 8 and full POC takes only 50k as compiled :)
+ */
+export class SimpleMatrixClient {
+
+    private readonly _observer     : Observer<SimpleMatrixClientEvent>;
+    private readonly _url          : string;
+    private readonly _accessToken  : string;
+    private readonly _userId       : string;
+    private readonly _pollTimeout     : number;
+    private readonly _pollWaitTime     : number;
+    private readonly _timeoutCallback : (() => void);
+
+    private _nextBatch : string;
+    private _timer     : any;
+    private _syncing   : boolean;
+
+    public constructor (
+        url: string,
+        accessToken: string,
+        userId: string,
+        pollTimeout : number = 30000,
+        pollWaitTime : number = 1000
+    ) {
+
+        this._url = url;
+        this._accessToken = accessToken;
+        this._userId = userId;
+        this._pollTimeout = pollTimeout;
+        this._pollWaitTime = pollWaitTime;
+        this._syncing = false;
+
+        this._observer = new Observer<SimpleMatrixClientEvent>("SimpleMatrixClient");
+
+        this._timeoutCallback = this._onTimeout.bind(this);
+
+    }
+
+    public static Event = SimpleMatrixClientEvent;
+
+    public destroy (): void {
+
+        this._observer.destroy();
+
+        if (this._timer !== undefined) {
+            clearTimeout(this._timer);
+            this._timer = undefined;
+        }
+
+    }
+
+    public on (
+        name: SimpleMatrixClientEvent,
+        callback: ObserverCallback<SimpleMatrixClientEvent>
+    ): SimpleMatrixClientDestructor {
+        return this._observer.listenEvent(name, callback);
+    }
+
+    public start () {
+
+        if (this._timer !== undefined) {
+            clearTimeout(this._timer);
+            this._timer = undefined;
+        }
+
+        this._initSync().catch((err : any) => {
+            LOG.error('SYNC ERROR: ', err);
+        });
+
+    }
+
+    public async sendTextMessage (roomId: string, body: string) : Promise<void> {
+
+        const requestBody : MatrixTextMessageDTO = {
+            msgtype: 'm.text',
+            body: body
+        };
+
+        LOG.debug(`Sending message with body:`, requestBody);
+
+        const response = await RequestClient.postJson(`${this._url}/_matrix/client/r0/rooms/${roomId}/send/m.room.message?access_token=${encodeURIComponent(this._accessToken)}`, requestBody as unknown as JsonAny);
+
+        LOG.debug(`Message sending response received: `, response);
+
+    }
+
+
+
+    private async _initSync () {
+
+        LOG.info(`Initial sync request started`);
+
+        const response = await RequestClient.getJson(`${this._url}/_matrix/client/r0/sync?filter={"room":{"timeline":{"limit":1}}}&access_token=${encodeURIComponent(this._accessToken)}`)
+
+        LOG.info(`Initial sync response received`);
+
+        // @ts-ignore
+        const next_batch : string | undefined = response?.next_batch;
+
+        if (next_batch) {
+            this._nextBatch = next_batch;
+        } else {
+            LOG.error(`No next_batch in the response: `, response)
+        }
+
+        this._timer = setTimeout(this._timeoutCallback, this._pollWaitTime);
+        LOG.info('Timer started...');
+
+    }
+
+    private async _syncSince (next: string) {
+
+        const response : any = await RequestClient.getJson(`${this._url}/_matrix/client/r0/sync?since=${encodeURIComponent(next)}&timeout=${this._pollTimeout}&access_token=${encodeURIComponent(this._accessToken)}`)
+
+        // @ts-ignore
+        const next_batch : string | undefined = response?.next_batch;
+        if (next_batch) {
+            this._nextBatch = next_batch;
+        } else {
+            LOG.error(`No next_batch in the response: `, response)
+        }
+
+        // LOG.debug('Response: ', response);
+
+        forEach(keys(response), (property : any) => {
+
+            const value : any = response[property];
+
+            // LOG.debug('value = ', value);
+
+            // @ts-ignore
+            const events : MatrixEventDTO[] = value?.events ?? [];
+
+            if (events) {
+                this._sendMatrixEventList(events, undefined);
+            }
+
+            const join = value?.join;
+            if (property === 'rooms' && join ) {
+                forEach(keys(join), (roomId : any ) => {
+                    const obj : any = join[roomId];
+                    // LOG.debug('obj = ', obj);
+
+                    forEach(keys(obj), (property : any) => {
+                        const roomValue = obj[property];
+
+                        const events : MatrixEventDTO[] | undefined = roomValue?.events;
+                        if ( events ) {
+                            this._sendMatrixEventList(events, roomId);
+                        // } else {
+                        //     LOG.debug('Room join: ', roomId, roomValue);
+                        }
+                    });
+
+                })
+            }
+
+        })
+
+
+    }
+
+    private _sendMatrixEventList (events : MatrixEventDTO[], room_id : string | undefined) {
+        forEach(events, (event) => {
+            this._sendMatrixEvent(event, room_id);
+        });
+    }
+
+    private _sendMatrixEvent (event : MatrixEventDTO, room_id : string | undefined) {
+        this._observer.triggerEvent(SimpleMatrixClientEvent.EVENT, room_id ? {...event, room_id} : event);
+    }
+
+    private _onTimeout () {
+
+        if (this._syncing) {
+            LOG.warn( `Warning! Already syncing...`);
+            return;
+        }
+
+        // LOG.info('On timeout...');
+
+        this._syncing = true;
+        this._syncSince(this._nextBatch).then(() => {
+
+            this._syncing = false;
+
+            if (this._timer !== undefined) {
+                clearTimeout(this._timer);
+                this._timer = undefined;
+            }
+
+            this._timer = setTimeout(this._timeoutCallback, this._pollWaitTime);
+            // LOG.info('Timer started again...');
+
+        }, (err) => {
+
+            this._syncing = false;
+            LOG.error(`ERROR: `, err);
+
+            if (this._timer !== undefined) {
+                clearTimeout(this._timer);
+                this._timer = undefined;
+            }
+
+            this._timer = setTimeout(this._timeoutCallback, this._pollWaitTime);
+            // LOG.info('Timer started again...');
+
+        });
+
+    }
+
+}
+
+export default SimpleMatrixClient;
