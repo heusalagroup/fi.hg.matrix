9b17f12d9c9d96d91a5998467e4394475c56b77f
Jaakko Heusala <jhh@heusalagroup.fi>
2021-12-10 21:24:52 +0200

    Initial command line interface class

---
+++ MatrixCrudCLI.ts
@@ -0,0 +1,14 @@
+// Copyright (c) 2021. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+
+export class MatrixCrudCLI<T> {
+
+    private _test   : TestCallback;
+    private _client : MatrixCrudRepository<T>;
+
+    public getAll () {
+
+    }
+
+}
+
+export default MatrixCrudCLI;
