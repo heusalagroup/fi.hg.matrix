d5d68b79e830be6520ba82a51c1d523fed856aad
Jaakko Heusala <jhh@heusalagroup.fi>
2022-04-24 11:25:06 +0300

    Added RoomGuestAccessStateEventDTO for https://github.com/heusalagroup/hghs/issues/22

---
+++ types/event/roomGuestAccess/RoomGuestAccessStateEventDTO.ts
@@ -0,0 +1,48 @@
+// Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+
+import {
+    isRoomGuestAccessContentDTO,
+    RoomGuestAccessContentDTO
+} from "./RoomGuestAccessContentDTO";
+import { MatrixStateEventOf } from "../../core/MatrixStateEventOf";
+import { MatrixType } from "../../core/MatrixType";
+import {
+    hasNoOtherKeysInDevelopment,
+    isRegularObject,
+    isString
+} from "../../../../core/modules/lodash";
+
+export type RoomGuestAccessStateEventDTO = MatrixStateEventOf<RoomGuestAccessContentDTO>;
+
+export function createRoomGuestAccessStateEventDTO (
+    content: RoomGuestAccessContentDTO
+): RoomGuestAccessStateEventDTO {
+    return {
+        type: MatrixType.M_ROOM_GUEST_ACCESS,
+        state_key: '',
+        content
+    };
+}
+
+export function isRoomGuestAccessStateEventDTO (value: any): value is RoomGuestAccessStateEventDTO {
+    return (
+        isRegularObject(value)
+        && hasNoOtherKeysInDevelopment(value, [
+            'type',
+            'state_key',
+            'content'
+        ])
+        && value?.type === MatrixType.M_ROOM_GUEST_ACCESS
+        && isString(value?.state_key)
+        && isRoomGuestAccessContentDTO(value?.content)
+    );
+}
+
+export function stringifyRoomGuestAccessStateEventDTO (value: RoomGuestAccessStateEventDTO): string {
+    return `RoomGuestAccessStateEventDTO(${value})`;
+}
+
+export function parseRoomGuestAccessStateEventDTO (value: any): RoomGuestAccessStateEventDTO | undefined {
+    if ( isRoomGuestAccessStateEventDTO(value) ) return value;
+    return undefined;
+}
