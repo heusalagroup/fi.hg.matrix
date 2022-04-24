5b0c6e22a4af8f5b6feb7c37c20dbd5d365861b2
Jaakko Heusala <jhh@heusalagroup.fi>
2022-04-24 13:00:51 +0300

    Added SynapsePreRegisterResponseDTO for https://github.com/heusalagroup/hghs/issues/1

---
+++ types/synapse/SynapsePreRegisterResponseDTO.ts
@@ -0,0 +1,32 @@
+// Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+
+import { hasNoOtherKeysInDevelopment, isRegularObject, isString } from "../../../core/modules/lodash";
+
+export interface SynapsePreRegisterResponseDTO {
+    readonly nonce : string;
+}
+
+export function createSynapsePreRegisterResponseDTO (
+    nonce : string
+): SynapsePreRegisterResponseDTO {
+    return {nonce};
+}
+
+export function isSynapsePreRegisterResponseDTO (value: any): value is SynapsePreRegisterResponseDTO {
+    return (
+        isRegularObject(value)
+        && hasNoOtherKeysInDevelopment(value, [
+            'nonce'
+        ])
+        && isString(value?.nonce)
+    );
+}
+
+export function stringifySynapsePreRegisterResponseDTO (value: SynapsePreRegisterResponseDTO): string {
+    return `SynapsePreRegisterResponseDTO(${value})`;
+}
+
+export function parseSynapsePreRegisterResponseDTO (value: any): SynapsePreRegisterResponseDTO | undefined {
+    if ( isSynapsePreRegisterResponseDTO(value) ) return value;
+    return undefined;
+}
