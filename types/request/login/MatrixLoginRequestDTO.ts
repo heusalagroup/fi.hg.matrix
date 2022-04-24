d4edbfffd34a767d35ee80760348023a2ef172d9
Jaakko Heusala <jhh@heusalagroup.fi>
2022-04-24 14:13:29 +0300

    Improved DTO for https://github.com/heusalagroup/hghs/issues/3

--- types/request/login/MatrixLoginRequestDTO.ts
+++ types/request/login/MatrixLoginRequestDTO.ts
@@ -1,14 +1,47 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 // Copyright (c) 2021. Sendanor <info@sendanor.fi>. All rights reserved.
 
-import { MatrixIdentifierDTO } from "./types/MatrixIdentifierDTO";
+import { isMatrixIdentifierDTO, MatrixIdentifierDTO } from "./types/MatrixIdentifierDTO";
 import { isMatrixLoginType, MatrixLoginType } from "./MatrixLoginType";
-import { hasNoOtherKeysInDevelopment, isRegularObject, isString } from "../../../../core/modules/lodash";
+import { hasNoOtherKeysInDevelopment, isRegularObject, isString, isStringOrUndefined, isUndefined } from "../../../../core/modules/lodash";
+import { MatrixUserId } from "../../core/MatrixUserId";
 
 export interface MatrixLoginRequestDTO {
-    readonly type        : MatrixLoginType;
-    readonly identifier ?: MatrixIdentifierDTO;
-    readonly password   ?: string;
+    readonly type                         : MatrixLoginType;
+    readonly identifier                  ?: MatrixIdentifierDTO;
+
+    /**
+     * Required when type is MatrixLoginType.M_LOGIN_PASSWORD
+     */
+    readonly password                    ?: string;
+
+    readonly device_id                   ?: string;
+
+    /**
+     * Use `identifier`
+     * @deprecated
+     */
+    readonly address                     ?: string;
+
+    readonly initial_device_display_name ?: string;
+
+    /**
+     * Use `identifier`
+     * @deprecated
+     */
+    readonly medium                      ?: string;
+
+    /**
+     * Required when type is MatrixLoginType.M_LOGIN_TOKEN
+     */
+    readonly token                       ?: string;
+
+    /**
+     * Use `identifier`
+     * @deprecated
+     */
+    readonly user                        ?: MatrixUserId | string;
+
 }
 
 export function isMatrixLoginRequestDTO (value: any): value is MatrixLoginRequestDTO {
@@ -17,9 +50,23 @@ export function isMatrixLoginRequestDTO (value: any): value is MatrixLoginReques
         && hasNoOtherKeysInDevelopment(value, [
             'type',
             'identifier',
-            'password'
+            'password',
+            'device_id',
+            'address',
+            'initial_device_display_name',
+            'medium',
+            'token',
+            'user'
         ])
-        && isMatrixLoginType(MatrixLoginType.M_LOGIN_PASSWORD)
+        && isMatrixLoginType(value?.type)
+        && (isUndefined(value?.identifier) || isMatrixIdentifierDTO(value?.identifier))
+        && ( value?.type === MatrixLoginType.M_LOGIN_PASSWORD ? isString(value?.password) : isStringOrUndefined(value?.password) )
+        && isStringOrUndefined(value?.device_id)
+        && isStringOrUndefined(value?.address)
+        && isStringOrUndefined(value?.initial_device_display_name)
+        && isStringOrUndefined(value?.medium)
+        && ( value?.type === MatrixLoginType.M_LOGIN_TOKEN ? isString(value?.token) : isStringOrUndefined(value?.token) )
+        && isStringOrUndefined(value?.user)
     );
 }
 
