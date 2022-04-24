60cd128227a910b625c6b58272f37c46bfdd502e
Jaakko Heusala <jhh@heusalagroup.fi>
2022-04-24 10:02:50 +0300

    Documented which types are for events

--- types/core/MatrixType.ts
+++ types/core/MatrixType.ts
@@ -5,27 +5,113 @@ import { isString } from "../../../core/modules/lodash";
 
 export enum MatrixType {
 
+    /**
+     * Event
+     */
     M_TEXT                      = 'm.text',
+
+    /**
+     * Not an event, part of the well known DTO response
+     */
     M_HOMESERVER                = 'm.homeserver',
+
+    /**
+     * Not an event, part of the well known DTO response
+     */
     M_IDENTITY_SERVER           = 'm.identity_server',
+
+    /**
+     * Part of the MatrixSyncResponseRoomSummaryDTO
+     */
     M_HEROES                    = 'm.heroes',
+
+    /**
+     * Part of the MatrixSyncResponseRoomSummaryDTO
+     */
     M_JOINED_MEMBER_COUNT       = 'm.joined_member_count',
+
+    /**
+     * Part of the MatrixSyncResponseRoomSummaryDTO
+     */
     M_INVITED_MEMBER_COUNT      = 'm.invited_member_count',
 
+    /**
+     * Event
+     */
     M_ROOM_MESSAGE              = 'm.room.message',
+
+    /**
+     * Event
+     */
     M_ROOM_POWER_LEVELS         = 'm.room.power_levels',
+
+    /**
+     * Event
+     */
     M_ROOM_JOIN_RULES           = 'm.room.join_rules',
+
+    /**
+     * Used as part of the MatrixType.M_ROOM_JOIN_RULES event.
+     *
+     * This may be written in wrong syntax, see https://github.com/heusalagroup/hghs/issues/20
+     */
     M_ROOM_MEMBERSHIP           = 'm.room.membership',
+
+    /**
+     * Event
+     */
     M_ROOM_HISTORY_VISIBILITY   = 'm.room.history_visibility',
+
+    /**
+     * Event
+     */
     M_ROOM_GUEST_ACCESS         = 'm.room.guest_access',
+
+    /**
+     * Event
+     */
     M_ROOM_CREATE               = 'm.room.create',
+
+    /**
+     * Part of room create event.
+     *
+     * See also MatrixRoomCreateEventDTO.
+     */
     M_FEDERATE                  = 'm.federate',
+
+    /**
+     * Event
+     */
     M_ROOM_MEMBER               = 'm.room.member',
+
+    /**
+     * Event
+     */
     M_PUSH_RULES                = 'm.push_rules',
+
+    /**
+     * Event
+     */
     M_PRESENCE                  = 'm.presence',
+
+    /**
+     * Room type
+     */
     M_SPACE                     = 'm.space',
+
+    /**
+     * Part of the login end point. See MatrixPasswordLoginDTO.
+     */
     M_LOGIN_PASSWORD            = 'm.login.password',
+
+    /**
+     * Part of the login end point.
+     */
     M_LOGIN_TOKEN               = 'm.login.token',
+
+    /**
+     * Part of the login end point. See also MatrixIdentifierDTO.
+     */
     M_ID_USER                   = 'm.id.user',
 
     FI_NOR_DELETED              = 'fi.nor.deleted',
