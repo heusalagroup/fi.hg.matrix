844408d1e83861c25bb39feed918f4d3496a847e
Jaakko Heusala <jhh@heusalagroup.fi>
2022-04-24 10:42:18 +0300

    Added type methods for https://github.com/heusalagroup/hghs/issues/17

--- types/message/textMessage/MatrixTextMessageDTO.ts
+++ types/message/textMessage/MatrixTextMessageDTO.ts
@@ -1,6 +1,39 @@
+// Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+
+import { hasNoOtherKeys, isRegularObject, isString } from "../../../../core/modules/lodash";
+import { isMatrixType, MatrixType } from "../../core/MatrixType";
+
 export interface MatrixTextMessageDTO {
+    readonly msgtype : MatrixType;
+    readonly body    : string;
+}
 
-    readonly msgtype: string;
-    readonly body: string;
+export function createMatrixTextMessageDTO (
+    body: string
+): MatrixTextMessageDTO {
+    return {
+        msgtype: MatrixType.M_TEXT,
+        body
+    };
+}
 
+export function isMatrixTextMessageDTO (value: any): value is MatrixTextMessageDTO {
+    return (
+        isRegularObject(value)
+        && hasNoOtherKeys(value, [
+            'msgtype',
+            'body'
+        ])
+        && isMatrixType(value?.msgtype)
+        && isString(value?.body)
+    );
+}
+
+export function stringifyMatrixTextMessageDTO (value: MatrixTextMessageDTO): string {
+    return `MatrixTextMessageDTO(${value})`;
+}
+
+export function parseMatrixTextMessageDTO (value: any): MatrixTextMessageDTO | undefined {
+    if ( isMatrixTextMessageDTO(value) ) return value;
+    return undefined;
 }
